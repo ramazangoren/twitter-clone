f0cf30f85e114d51c5b1eb3b09357ff72a0c1bf0
ramazangoren <rmzngrn0404@gmail.com>
2024-07-25 02:36:03 +0300

    twitter-clone auth added

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,17 +1,13 @@
 import express from "express";
-// import { getMe, login, logout, signup } from "../controllers/auth.controller.js";
-import { signup, login, logout } from "../controllers/auth.controller.js";
-// import { protectRoute } from "../middleware/protectRoute.js";
+import { getMe, login, logout, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-// router.get("/me", protectRoute, getMe);
-// router.post("/signup", signup);
-// router.post("/login", login);
-// router.post("/logout", logout);
 
 export default router;
