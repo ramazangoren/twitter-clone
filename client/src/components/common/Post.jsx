cc62354511ada14cf84509b8f9a725da9e61e6c9
ramazangoren <rmzngrn0404@gmail.com>
2024-07-27 16:50:56 +0300

    comment and like added

--- client/src/components/common/Post.jsx
+++ client/src/components/common/Post.jsx
@@ -8,6 +8,8 @@ import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
 
+import axios from 'axios'
+
 import LoadingSpinner from "./LoadingSpinner";
 import { formatPostDate } from "../../utils/date";
 
